575cd0e6dabb786391c5bbe7bebb1ce6836bd1fa
Jamiu Mojolagbe <jamiumojolagbe@Mojolagbe.local>
2018-05-21 08:49:38 -0500

    initial commit

---
+++ public/scripts/custom.js
@@ -0,0 +1,136 @@
+$(document).ready(function(){
+    var thisPet = $('#temp-details');
+    var messageBox = $('.message-box');
+    var responseBox = $('#weather-response');
+    var thisURL = window.location.href;
+    var addForm = $("form");
+    
+    
+    
+    if (thisURL.indexOf("/add") < 0){
+        checkWeather(thisPet, messageBox, responseBox);
+    }
+    
+    if (thisURL.indexOf("/add") >= 0){
+        getLocation(addForm);
+    }
+    
+    $('#latitude, #longitude').on('change paste keyup', function() {
+        var latit = $('#latitude').val();
+        var longit = $('#longitude').val();
+        reverseGeoLocation(latit, longit, addForm);
+    });
+    
+    $("form#AddPet").submit(function(e){ 
+        e.stopPropagation();
+        e.preventDefault();
+        
+        var formData = {};
+        $('#AddPet :input').each(function() {
+            if(this.name !="") formData[this.name] = $(this).val();
+        });
+        //console.log(formData);
+        //var testData = {name: 'name', type: 'type', breed: 'breed', location:'location', latitude:90, longitude:-90};
+        
+        $.ajax({
+            url: $(this).attr("action"),
+            type: 'POST',
+            dataType: 'json',
+            data: formData,
+            beforeSend: function() {
+                messageBox.empty().html(('<img src="public/images/loading.gif" width="50" height="50" alt="Loading..."/>'));
+            },
+            success : function(data, status) {
+                var contents = data.message ? data.message : data;
+                if(data.status ===1) { 
+                    messageBox.empty().html($('<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">&times;</button>'+contents+'  </div>')); 
+                }
+                else messageBox.empty().html($('<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>'+contents +'</div>'));
+            },
+            error : function(xhr, status) {
+                erroMsg = '';
+                if(xhr.status===0){ erroMsg = 'There is a problem connecting to internet. Please review your internet connection.'; }
+                else if(xhr.status===404){ erroMsg = 'Requested page not found.'; }
+                else if(xhr.status===500){ erroMsg = 'Internal Server Error.';}
+                else if(status==='parsererror'){ erroMsg = 'Error. Parsing JSON Request failed.'; }
+                else if(status==='timeout'){  erroMsg = 'Request Time out.';}
+                else { erroMsg = 'Unknow Error.\n'+xhr.responseText;}          
+                messageBox.empty().html($('<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Pet not added.\n '+erroMsg+'</div>'));
+            }
+        });
+        
+    });
+});
+
+function checkWeather(pet, messageBox, responseBox){
+    const apiKey = '752ca7ee5d3924bfb68660d1ad417709';
+    const api_url = 'https://api.darksky.net/forecast/'+apiKey+'/'+pet.attr('data-latitude')+','+pet.attr('data-longitude');
+    
+    $.ajax(api_url, {
+        type:"GET",
+        dataType:"jsonp",
+        data:{}, 
+        beforeSend: function() {
+            responseBox.addClass('hidden').hide(); 
+            messageBox.show().html('<img src="public/images/loading.gif" alt="Loading..."/>');
+        },
+        success:function(data, status, jqXHR) {
+            messageBox.hide();
+            responseBox.removeClass('hidden').show();
+            console.log(data);
+            if(data.hourly.icon === 'rain'){
+                responseBox.find('h3').text('It looks like '+pet.attr('data-name')+' is going to need one in '+pet.attr('data-location')+'.');
+                responseBox.find('h2').text('Yup!');
+                responseBox.find('img').attr('src', 'public/images/cat-holding-umbrella.png');
+                responseBox.find('#alert-head').addClass('alert-warning');
+            } else {
+                responseBox.find('h3').text('It looks like '+pet.attr('data-name')+' does not need one in '+pet.attr('data-location')+'.');
+                responseBox.find('h2').text('Nope!');
+                responseBox.find('img').attr('src', 'public/images/cat-no-umbrella.png');
+                responseBox.find('#alert-head').addClass('alert-info');
+            }
+        },
+        error: function(xhr, status, error) {console.log(error);;}
+    });
+}
+
+function getLocation(selector) {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position){
+            var latitude = position.coords.latitude,
+                longitude = position.coords.longitude;
+                
+            selector.find('#latitude').val(latitude);
+            selector.find('#longitude').val(longitude);
+            reverseGeoLocation(latitude, longitude, selector);
+        });
+    } else {
+        selector.find('.message-box').html('Geolocation not supported!!!');
+    }
+}
+
+function reverseGeoLocation(latitude, longitude, selector) {
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url:'https://maps.googleapis.com/maps/api/geocode/json?latlng='+latitude+','+longitude+'&sensor=true&key=AIzaSyDQ27WgInDdmdUlbeM_-CsTmfY_Jx0LCyg',
+        success: function(data)  {
+            //console.log(data);
+            var city = '';
+            var province = '';
+            
+            for (var i = 0; i < data.results[4].address_components.length; i++) {
+                for (var j = 0; j < data.results[4].address_components[i].types.length; j++) {
+                    if(data.results[4].address_components[i].types[j] === 'locality') {
+                        city = data.results[4].address_components[i].long_name;
+                    }
+                    if(data.results[4].address_components[i].types[j] === 'administrative_area_level_1') {
+                        province = data.results[4].address_components[i].short_name;
+                    }
+                }
+            }
+            //selector.find('#location1').val(city+', '+province);
+            selector.find('#location').val(city+', '+province);
+        }
+    });  
+}
---
+++ server.js
@@ -0,0 +1,175 @@
+'use strict';
+
+var config = {
+    env: "development",
+    host: "localhost",
+    port: 3000,
+    charset: "utf8",
+    restURL: "http://localhost:8000/"
+};
+
+const author = {
+    name: "Jamiu Mojolagbe",
+    email: "mojolagbe@gmail.com",
+    phone: "2048697315",
+    GoogleAPIKey: "AIzaSyDQ27WgInDdmdUlbeM_-CsTmfY_Jx0LCyg",
+    DarkSkyAPIKey: "752ca7ee5d3924bfb68660d1ad417709"
+};
+
+const Hapi = require('hapi');   
+const server = new Hapi.Server();
+const req = require('request');
+const Joi = require('joi');
+
+var connecParams = (config.env === 'production')  ?   { port: process.env.PORT }
+                : {port: config.port, host:config.host};
+
+server.connection(connecParams);
+
+const start = async () => {
+    server.register(
+        [  
+            {
+              register: require('vision')  // add template rendering engine
+            },
+            {
+              register: require('inert')  // handler for static files/directories
+            }
+        ], 
+        function(error) {
+            if (error) { throw error; }
+
+            // views configuration
+            server.views({  
+                engines: {
+                    html: require('handlebars')
+                },
+                relativeTo: __dirname,
+                path: 'views',
+                layoutPath: 'views/layout',
+                layout: 'default',
+                //helpersPath: 'views/helpers',
+                partialsPath: 'views/partials'
+            });
+        }
+    );
+};
+start();
+
+
+// --------------
+// Load Routes
+// --------------
+
+// home page
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function(request, response) {
+        //response.view('index');
+        req(config.restURL+'pets/', function (err, resp, cont) {
+            if (!err && resp.statusCode === 200) {
+                response.view('index', {status: resp.statusCode, data: JSON.parse(cont)});
+            } else {
+                throw err;
+                console.log(err);
+                response.view('index', {status: resp.statusCode, data: JSON.parse(err)});
+            }
+        });
+    }
+});
+
+// author's page
+server.route({
+    method: 'GET',
+    path: '/author',
+    handler: function(request, response) {
+        var data = {
+            name: author.name,
+            phone: author.phone,
+            email: author.email
+        };
+        response.view('author', data);
+    }
+});
+
+// add pet page
+server.route({
+    method: 'GET',
+    path: '/add',
+    handler: function(request, response) {
+        response.view('pet', {status: 1, actionURL: config.restURL+'pets/'});
+    }
+});
+
+// pet's details page
+server.route({
+    method: 'GET',
+    path: '/{id}',
+    handler: function(request, response) {
+        //response.view('pets', {status: 0, data: 1});
+        if(request.params.id > 0) {
+            req(config.restURL+'pets/'+request.params.id+'/', function (err, resp, cont) {
+                if (!err && resp.statusCode === 200) {
+                    //console.log({status: resp.statusCode, data: JSON.parse(cont)});
+                    response.view('pets', {status: resp.statusCode, data: JSON.parse(cont)});
+                } else {
+                    throw err;
+                    console.log(err);
+                    response.view('pets', {status: resp.statusCode, data: err});
+                }
+            });
+        }
+    },
+    config: {
+        validate: {
+            params: {
+                id: Joi.number().integer().min(1)
+            }
+        }
+    }
+});
+
+
+
+
+// --------------------
+// Static file routes
+// ---------------------
+    
+server.route({
+    method: 'GET',
+    path: '/public/css/{style*}',
+    handler: (request, reply) => {
+        reply.file(`public/css/${request.params.style}`);
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/public/images/{imgfile*}',
+    handler: (request, reply) => {
+        reply.file(`public/images/${request.params.imgfile}`);
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/public/scripts/{scriptfile*}',
+    handler: (request, reply) => {
+        reply.file(`public/scripts/${request.params.scriptfile}`);
+    }
+});
+
+
+
+
+// ------------------
+// Start The Server
+// ------------------
+server.start(error => {  
+    if (error) {
+        console.error(error);
+    }
+    console.log(`Server started at ${server.info.uri }`);
+});
